f57139a50682c30f7c0df9d3dbddf0187aab2c73
Néstor Batista <nestor.batista@otsugroup.es>
2024-05-22 09:19:50 +0100

    install java

---
+++ install_java.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Descargar e instalar OpenJDK 11
+wget https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
+tar -xvf openjdk-11+28_linux-x64_bin.tar.gz
+mv jdk-11 /opt/jdk-11
+
+# Establecer JAVA_HOME y actualizar PATH
+export JAVA_HOME=/opt/jdk-11
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Verificar instalación de Java
+java -version
